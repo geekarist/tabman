3fed60bceeca59d3a23b2936ee6ec54472706c87
Christophe <christophe.pele@gmail.com>
2012-12-11 21:35:26 +0100

    Create page with 2 songs, using angular.js and 1 controller, with data stored in a js variable

---
+++ tabman.js
@@ -0,0 +1,6 @@
+function TabManController($scope) {
+    $scope.allTabs = [
+        { artist:"Radiohead", title:"Creep" },
+        { artist:"Johnny Cash", title:"I'm so lonesome I could cry" }
+    ];
+}
---
+++ test.js
@@ -0,0 +1,26 @@
+var soda = require('soda'),
+    assert = require('assert');
+
+var browser = soda.createClient({
+    host: 'localhost',
+    port: 4444,
+    url: 'http://localhost:8888/tabman',
+    browser: 'safari'
+});
+
+browser.on('command', function(cmd, args){
+    console.log(' \x1b[33m%s\x1b[0m: %s', cmd, args.join(', '));
+});
+
+browser
+    .chain
+    .session()
+    .open('/tabman')
+    .assertText('//li[1]', 'Creep, interprété par Radiohead')
+    .assertText('//li[2]', "I'm so lonesome I could cry, interprété par Johnny Cash")
+    .end(function(err){
+        browser.testComplete(function() {
+            console.log('done');
+            if (err) throw err;
+        });
+    });
