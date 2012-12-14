3ed9a5b3b7f17fde7c123701b4dddc7a620f2bdb
Christophe <christophe.pele@gmail.com>
2012-12-14 19:07:13 +0100

    Add songs to data, setup twitter bootstrap css

--- tabman.js
+++ tabman.js
@@ -1,6 +1,8 @@
 function TabManController($scope) {
     $scope.allTabs = [
         { artist:"Radiohead", title:"Creep" },
-        { artist:"Johnny Cash", title:"I'm so lonesome I could cry" }
+        { artist:"Johnny Cash", title:"I'm so lonesome I could cry" },
+        { artist:"Bob Dylan", title:"Blowin' in the wind" },
+        { artist:"Bob Dylan", title:"It ain't me babe" }
     ];
 }
